336679f58f774d538c5f589797d2131356ec896c
xavier <montjuic79@gmail.com>
2023-07-29 18:42:58 +0200

    aplicacion ls

---
+++ 5.ls.js
@@ -0,0 +1,13 @@
+const fs = require('node:fs')
+
+//haz un ls en el directorio actual '.'
+fs.readdir('.', (err, files) => {
+    if(err){
+        console.error('Error al leer el directorio');
+        return
+    }
+
+    files.forEach(file => {
+        console.log(file)
+    })
+})
