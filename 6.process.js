f7e81447d00067a4e40ca8f498b8d0de9582da74
xavier <montjuic79@gmail.com>
2023-07-29 18:54:19 +0200

    objeto process

---
+++ 6.process.js
@@ -0,0 +1,16 @@
+// argumentos de entrada
+//console.log(process.argv)
+
+// controlar el proceso y su salida
+//process.exit(0)
+
+// podemos controlar eventos del proceso
+process.on('exit', () => {
+    //limpiar recursos
+})
+
+// current working directory
+console.log(process.cwd())
+
+//variables entorno
+console.log(process.env.PEPITO)
