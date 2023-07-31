da34e2946e3df727680357922a1c629e0dd5512c
xavier <montjuic79@gmail.com>
2023-07-31 13:00:46 +0200

    Aplicación puerto

--- 8.http.js
+++ 8.http.js
@@ -1,11 +1,16 @@
 /* eslint-disable indent */
 const http = require('node:http')
+const { findAvalaiblePort } = require('./9.free-port.js')
+
+const desiredPort = process.env.PORT ?? 3000
 
 const server = http.createServer((req, res) => {
     console.log('request received')
     res.end('Hola mundo')
 })
 
-server.listen(0, () => {
-    console.log(`server listening on port http://localhost:${server.address().port}`)
+findAvalaiblePort(desiredPort).then(port => {
+    server.listen(port, () => {
+        console.log(`server listening on port http://localhost:${server.address().port}`)
+    })
 })
